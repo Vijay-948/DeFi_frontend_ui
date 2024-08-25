a56dc46ae305e137ccabcb22a2b590b04213ab10
Vijay-948 <kvijayreddy948@gmail.com>
2024-08-25 23:34:44 +0530

    modules

---
+++ src/constants/common.ts
@@ -0,0 +1,11 @@
+export const ROLES = {
+  ADMIN: "admin",
+  USER: "user",
+};
+
+export const MODULE = {
+  DASHBOARD: "Dashboard",
+  HOME: "Home",
+  SETTINGS: "Settings",
+  PROFILE: "Profile",
+};
